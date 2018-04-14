3e64870c4d564617bbe6b45ad504651a9c8be28e
zhaoyunxing92 <2385585770@qq.com>
2018-04-15 01:13:08 +0800

    添加：controller

---
+++ spring-cloud-eureka/eureka-provider/src/main/java/com/sunny/springcloud/springcloudeurekaprovider/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.sunny.springcloud.springcloudeurekaprovider.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sunny
+ * @className HelloController
+ * @date 2018-04-15 01:06
+ * @description:
+ */
+@RestController
+public class HelloController {
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/say")
+    String sayHello(@RequestParam String name) {
+        return "hello " + name + " i am from port:" + port;
+    }
+}
